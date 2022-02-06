2dd2f2f6175d1ce686d19a4749c318fd190f6848
Dave Sugden <davelms@gmail.com>
2022-02-06 16:29:19 -0500

    Update message in Firebase Function

--- firebase-functions-actions/functions/index.js
+++ firebase-functions-actions/functions/index.js
@@ -5,5 +5,5 @@ const functions = require("firebase-functions");
 //
 exports.helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+  response.send("Hello from Firebase version 2!");
 });
